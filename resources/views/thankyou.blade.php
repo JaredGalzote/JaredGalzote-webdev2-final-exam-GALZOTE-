2f96c1faf6c8b3d49262bf9698b9a53eaace8ade
Jared Galzote <71679362+JaredGalzote@users.noreply.github.com>
2021-05-19 08:34:14 +0800

    Update thankyou.blade.php

--- resources/views/thankyou.blade.php
+++ resources/views/thankyou.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 <div class="jumbotron text-center">
   <h1 class="display-3">Thank You!</h1>
-  <p class="lead">You Request have successfully sent!</p>
+  <p class="lead">Your Request have successfully sent!</p>
   <hr>
   <p class="lead">
     <a class="btn btn-primary btn-sm" href="/bible-study-requests" role="button">Check your Request</a>
